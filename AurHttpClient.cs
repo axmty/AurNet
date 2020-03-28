28f5d628efe8d363de8861cf8538b71f02cc1439
axmty <alexandre.metgy@gmail.com>
2020-03-28 13:43:42 +0100

    Init ClientUrlBuilder classes for 'search' and 'info' types.

--- AurHttpClient.cs
+++ AurHttpClient.cs
@@ -1,24 +1,53 @@
 using System;
+using System.Collections.Specialized;
 using System.Net.Http;
+using System.Web;
 
-namespace AurNet
+namespace AurNet.AurHttpClient
 {
-    public static class AurHttpClientConfig
+    public class SearchTypeUrlBuilder : ClientUrlBuilder
     {
-        public const string Baseurl = "https://aur.archlinux.org/rpc/";
-        public const int Version = 5;
-        public const string SearchTypeName = "search";
-        public const string InfoTypeName = "info";
+        public override string TypeQueryParamValue => "search";
+    }
+
+    public class InfoTypeUrlBuilder : ClientUrlBuilder
+    {
+        public override string TypeQueryParamValue => "info";
+    }
+
+    public abstract class ClientUrlBuilder
+    {
+        private const string BaseUrl = "https://aur.archlinux.org/rpc/";
+        
+        private const int Version = 5;
+        
+        private const string TypeQueryParamKey = "type";
+        
+        private const string VersionQueryParamKey = "v";
+        
+        public abstract string TypeQueryParamValue { get; }
+
+        protected abstract NameValueCollection GetTypeQueryParams();
+
+        public string Build()
+        {
+            var builder = new UriBuilder(BaseUrl);
+            builder.Port = -1;
+
+            var query = HttpUtility.ParseQueryString(builder.Query);
+            query[VersionQueryParamKey] = Version.ToString();
+            query[TypeQueryParamKey] = TypeQueryParamValue;
+            builder.Query = query.ToString();
+
+            return builder.ToString();
+        }
     }
 }
 
-namespace AurNet
+namespace AurNet.AurHttpClient
 {
     public class AurHttpClient
     {
-        private static readonly HttpClient HttpClient = new HttpClient()
-        {
-            BaseAddress = new Uri(AurHttpClientConfig.Baseurl)
-        };
+        private static readonly HttpClient Client = new HttpClient();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace AurNet
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
