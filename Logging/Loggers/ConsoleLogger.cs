534c82cce7cf5f424c15c6ef7968e6d4d3718b83
axmty <alexandre.metgy@gmail.com>
2020-03-29 18:27:51 +0200

    Add color handling in ConsoleLogger.

--- Logging/Loggers/ConsoleLogger.cs
+++ Logging/Loggers/ConsoleLogger.cs
@@ -17,8 +17,23 @@ namespace AurNet.Logging
         
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
+            Console.ForegroundColor = ColorFromLogLevel(logLevel);
+
             var message = formatter(state, exception);
             Console.WriteLine(message);
+            
+            Console.ResetColor();
+        }
+
+        private ConsoleColor ColorFromLogLevel(LogLevel logLevel)
+        {
+            return logLevel switch
+            {
+                LogLevel.Warning => ConsoleColor.Yellow,
+                LogLevel.Error => ConsoleColor.Red,
+                LogLevel.Critical => ConsoleColor.DarkRed,
+                _ => ConsoleColor.White,
+            };
         }
     }
 }
--- Logging/Loggers/FileLogger.cs
+++ Logging/Loggers/FileLogger.cs
@@ -18,7 +18,6 @@ namespace AurNet.Logging
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
             var message = formatter(state, exception);
-            // Console.WriteLine("FILE: " + message);
         }
     }
 }
