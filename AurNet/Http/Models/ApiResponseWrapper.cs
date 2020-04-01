e54795dd7a4e21fcfe84d3aaf5d8dad4332c65f3
axmty <alexandre.metgy@gmail.com>
2020-04-01 21:10:59 +0200

    Write XML doc for ApiResponseWrapper class.

--- AurNet/Http/Models/ApiResponseWrapper.cs
+++ AurNet/Http/Models/ApiResponseWrapper.cs
@@ -1,13 +1,29 @@
 namespace AurNet.Http
 {
+    /// <summary>
+    /// Wrapper that can hold either a success or an error response.
+    /// </summary>
     public class ApiResponseWrapper<TSuccessResponse>
     {
+        /// <summary>
+        /// Success response.
+        /// </summary>
         public TSuccessResponse SuccessResponse { get; set; }
 
+        /// <summary>
+        /// Error response.
+        /// </summary>
+        /// <value></value>
         public ApiErrorResponse ErrorResponse { get; set; }
 
+        /// <summary>
+        /// Returns true if and only if it is a success response.
+        /// </summary>
         public bool IsSuccess => this.ErrorResponse != null;
 
+        /// <summary>
+        /// Returns true if and only if it is an error response.
+        /// </summary>
         public bool IsError => !this.IsSuccess;
     }
 }
