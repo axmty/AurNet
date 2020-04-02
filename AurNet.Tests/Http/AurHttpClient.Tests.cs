2ec749abf42674a82bb5a269395fd1db93db7f37
axmty <alexandre.metgy@gmail.com>
2020-04-03 00:26:10 +0200

    Unit test AurHttpClient.SearchAsync in case of technical exception.

---
+++ AurNet.Tests/Http/AurHttpClient.Tests.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using AurNet.Http;
+using Moq;
+using Xunit;
+
+namespace AurNet.Tests.Http
+{
+    public class AurHttpClientTests
+    {
+        [Fact]
+        public async Task SearchAsync_ReturnsFunctionalErrorResponse_WhenApiResponseContainsErrorMessage()
+        {
+            var expectedApiResponse = @"
+            {
+                ""version"":5,
+                ""type"":""error"",
+                ""resultcount"":0,
+                ""results"":[],
+                ""error"":""Incorrect by field specified.""
+            }";
+            var aurClient = new AurHttpClient(MockIHttpClientFactoryWithRawResponse(expectedApiResponse));
+
+            var responseWrapper = await aurClient.SearchAsync("", SearchField.NameDesc);
+
+            Assert.True(responseWrapper.IsError);
+            Assert.True(responseWrapper.ErrorResponse.IsFunctional);
+            Assert.Equal("Incorrect by field specified.", responseWrapper.ErrorResponse.FunctionalErrorMessage);
+        }
+
+        [Fact]
+        public async Task SearchAsync_ReturnsTechnicalErrorResponse_WhenExceptionIsRaisedWhenCallingApi()
+        {
+            var raisedException = new HttpRequestException();
+            var aurClient = new AurHttpClient(MockIHttpClientFactoryWithExceptionRaised(raisedException));
+
+            var responseWrapper = await aurClient.SearchAsync("", SearchField.NameDesc);
+
+            Assert.True(responseWrapper.IsError);
+            Assert.True(responseWrapper.ErrorResponse.IsTechnical);
+            Assert.Equal(raisedException, responseWrapper.ErrorResponse.TechnicalException);
+        }
+
+        private static IHttpClientFactory MockIHttpClientFactoryWithRawResponse(string rawResponse)
+        {
+            var mockHandler = new MockDelegatingHandler(() => new HttpResponseMessage
+            {
+                Content = new StringContent(rawResponse)
+            });
+
+            return MockIHttpClientFactoryWithDelegatingHandler(mockHandler);
+        }
+
+        private static IHttpClientFactory MockIHttpClientFactoryWithExceptionRaised(Exception exception)
+        {
+            var mockHandler = new MockDelegatingHandler(() => throw exception);
+
+            return MockIHttpClientFactoryWithDelegatingHandler(mockHandler);
+        }
+
+        private static IHttpClientFactory MockIHttpClientFactoryWithDelegatingHandler(MockDelegatingHandler handler)
+        {
+            var mockClient = new HttpClient(handler);
+            var mockFactory = new Mock<IHttpClientFactory>();
+            mockFactory.Setup(factory => factory.CreateClient(It.IsAny<string>())).Returns(mockClient);
+
+            return mockFactory.Object;
+        }
+
+        public class MockDelegatingHandler : DelegatingHandler
+        {
+            private readonly Func<HttpResponseMessage> _handler;
+
+            public MockDelegatingHandler(Func<HttpResponseMessage> handler)
+            {
+                _handler = handler;
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(
+                HttpRequestMessage request,
+                CancellationToken cancellationToken)
+            {
+                return Task.FromResult(_handler());
+            }
+        }
+    }
+}
